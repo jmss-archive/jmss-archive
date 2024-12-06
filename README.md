9aba727cf448824b351d49a3c274169098af9ac0
Joel <58995538+jtpotato@users.noreply.github.com>
2024-12-06 21:58:59 +1100

    restart

--- README.md
+++
@@ -1,5 +0,0 @@
-# jmss-archive
-
-A place for very important JMSS documents
-
-Using SvelteKit
--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import '@fontsource/roboto';
-	import '@fontsource/playfair-display';
-	import '@fontsource/ibm-plex-serif';
-	import '$lib/styles.css';
-</script>
-
-<slot />
